cf365e0e71a72fe73e48d5471805756212e4d7cf
Song <song@SongtekiMacBook-Pro.local>
2012-12-12 01:50:29 +0100

    TumuXiaozhan added and it is synced with server

---
+++ TongjiXuanke/TumuXiaozhanModel.h
@@ -0,0 +1,13 @@
+//
+//  TumuXiaozhanModel.h
+//  TongjiXuanke
+//
+//  Created by Song on 12-12-12.
+//  Copyright (c) 2012年 Song. All rights reserved.
+//
+
+#import "ServerRSSNewsModel.h"
+
+@interface TumuXiaozhanModel : ServerRSSNewsModel
+
+@end
---
+++ TongjiXuanke/TumuXiaozhanModel.m
@@ -0,0 +1,22 @@
+//
+//  TumuXiaozhanModel.m
+//  TongjiXuanke
+//
+//  Created by Song on 12-12-12.
+//  Copyright (c) 2012年 Song. All rights reserved.
+//
+
+#import "TumuXiaozhanModel.h"
+
+@implementation TumuXiaozhanModel
+
+-(id)init
+{
+    if(self = [super init])
+    {
+        categoryOnServer = @"tumu";
+    }
+    return self;
+}
+
+@end
