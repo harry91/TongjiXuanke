548f6d80f9cd6d15b6022d33d0fe086242d8ca20
Song <song@SongtekiMacBook-Pro.local>
2013-01-27 22:32:28 +0800

    share sdk?

---
+++ TongjiXuanke/libs/ShareSDK_v1.0.2/ShareSDK.framework/Headers/ShareConfig.h
@@ -0,0 +1,77 @@
+//
+//  AGShareConfig.h
+//  SNSServiceSDK
+//
+//  Created by ShareSDK.cn on 13-1-14.
+//  官网地址:http://www.ShareSDK.cn
+//  技术支持邮箱:support@sharesdk.cn
+//  官方微信:ShareSDK   （如果发布新版本的话，我们将会第一时间通过微信将版本更新内容推送给您。如果使用过程中有任何问题，也可以通过微信与我们取得联系，我们将会在24小时内给予回复）
+//  商务QQ:1955211608
+//  Copyright (c) 2013年 ShareSDK.cn. All rights reserved.
+//
+
+#ifndef SNSServiceSDK_AGShareConfig_h
+#define SNSServiceSDK_AGShareConfig_h
+
+#pragma mark - 新浪微博应用配置信息
+//http://open.weibo.com上注册新浪微博开放平台应用，并将相关信息填写到以下字段
+
+NSString *ssSinaAppKey = @"846671969";
+NSString *ssSinaAppSecret = @"683e29cf5d1ebeeb0c705fb0e972b3f0";
+NSString *ssSinaRedirectUri = @"http://";
+
+#pragma mark - 腾讯微博应用配置信息
+//http://dev.t.qq.com上注册腾讯微博开放平台应用，并将相关信息填写到以下字段
+
+NSString *ssTecentAppKey = @"0";
+NSString *ssTecentAppSecret = @"0";
+NSString *ssTecentRedirectUri = @"0";
+
+
+#pragma mark - QQ空间应用配置信息
+//http://connect.qq.com/intro/login/上申请加入QQ登录，并将相关信息填写到以下字段
+
+NSString *ssQqSpaceAppKey = @"100369045";
+NSString *ssQqSpaceAppSecret = @"1322ac8fcb0d6cd73a4b7da7722f4032";
+NSString *ssQqSpaceRedirectUri = @"auth://tauth.qq.com/";
+
+#pragma mark - 网易微博应用配置信息
+//http://open.t.163.com上注册网易微博开放平台应用，并将相关信息填写到以下字段
+
+NSString *ssT163AppKey = @"0";
+NSString *ssT163AppSecret = @"0";
+NSString *ssT163RedirectUri = @"http://www.appgo.cn";
+
+#pragma mark - 搜狐微博应用配置信息
+//http://open.t.sohu.com上注册搜狐微博开放平台应用，并将相关信息填写到以下字段
+
+NSString *ssSohuConsumerKey = @"0";
+NSString *ssSohuConsumerSecret = @"0";
+
+#pragma mark - 豆瓣社区应用配置信息
+//http://developers.douban.com上注册豆瓣社区应用，并将相关信息填写到以下字段
+
+NSString *ssDoubanAppKey = @"0";
+NSString *ssDoubanAppSecret = @"0";
+NSString *ssDoubanRedirectUri = @"http://www.appgo.cn";
+
+#pragma mark - 人人网应用配置信息
+//http://dev.renren.com上注册人人网开放平台应用，并将相关信息填写到以下字段
+
+NSString *ssRenrenAppKey = @"f75a5aad71904ed0a42a9cd694192836";
+NSString *ssRenrenAppSecret = @"d6254ea8ec694502a3f5a4c4d0aa460a";
+
+#pragma mark - 开心网应用配置信息
+//http://open.kaixin001.com上注册开心网开放平台应用，并将相关信息填写到以下字段
+
+NSString *ssKaixinAppKey = @"0";
+NSString *ssKaixinAppSecret = @"0";
+NSString *ssKaixinRedirectUri = @"http://www.appgo.cn/";
+
+#pragma mark - Instapaper应用配置信息
+//http://www.instapaper.com/main/request_oauth_consumer_token上注册Instapaper应用，并将相关信息填写到以下字段
+
+NSString *ssInstapaperConsumerKey = @"0";
+NSString *ssInstapaperConsumerSecret = @"0";
+
+#endif
